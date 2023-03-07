85532e8256f55c1dd96e68298a4e20430952c1f6
GattiDev <gattidev323@gmail.com>
2023-03-07 10:15:29 -0300

    Primer Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?PHP 
+    include ('conexion/bd.php'); //--> Importo la Base de Dato
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<!-- Para que funcione en los navegadores viejos -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- Para los celulares, para que no hagan zoom -->
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<title>GattiDev</title>
+		<!-- Icon -->
+		<link rel="icon" type="favicon/x-icon" href="<?php echo ICON_URL; ?>favicon.ico" />
+		<!-- Styles -->
+		<link rel="stylesheet" type="text/css" href="<?php echo CSS_URL; ?>styles.css" />
+		<!-- Font Awesome V6 -->
+		<script src="https://kit.fontawesome.com/e28972e8bb.js" crossorigin="anonymous"></script>
+		
+		
+	</head>
+	<body>
+		<!-- Head Content - Inicio -->
+		<header id="head_content">
+			<header>
+			</header>
+			<nav></nav>
+		</header> 
+		<!-- Head Content - Fin -->
+		<!-- Main Content - Inicio -->
+		<main id="main_content">
+			<section>
+				<article></article>
+			</section>
+			<aside></aside>		
+		</main>
+		<!-- Main Content - Fin -->
+		<!-- Footer - Inicio -->
+		<footer id="footer_content">
+			<span></span>
+		</footer>
+		<!-- Footer - Fin -->
+	</body>
+</html>
